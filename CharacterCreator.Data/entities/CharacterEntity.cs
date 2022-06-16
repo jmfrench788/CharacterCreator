f7ef4fe1e19d7363a262d550faa39a7e21498dd9
Rob Rupp <robruppjr@gmail.com>
2022-06-16 18:11:05 -0400

    CharacterEntity Added

--- CharacterCreator.Data/entities/CharacterEntity.cs
+++ CharacterCreator.Data/entities/CharacterEntity.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-namespace CharacterCreator.Data.entities
-{
-    public class CharacterEntity
+using CharacterCreator.Data.entities;
+public class CharacterEntity
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Race Race { get; set; }
+        public List<SkillEntity> Skills { get; set; }
+        public TeamEntity  Team { get; set; }
+        public int StrengthStat { get; set; }
+        public Class Class { get; set; }
     }
-}
