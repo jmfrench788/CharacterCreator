d6067efc34acb0319eaae9c3af032339e48ef73b
Julia French <jmfrench788@gmail.com>
2022-06-15 21:23:28 -0400

    Added ApplicationDbContext

--- CharacterCreator.WebAPI/Program.cs
+++ CharacterCreator.WebAPI/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "CharacterCreator.WebAPI", Version = "v1"});
+});
 
 var app = builder.Build();
 
