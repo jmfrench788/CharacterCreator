ae9484acc61f0f6aa3b189262516c54db5a3822d
Robruppjr <99684433+Robruppjr@users.noreply.github.com>
2022-06-16 19:46:19 -0400

    Revert "SetUp DBContext"

--- CharacterCreator.Data/ApplicationDbContext.cs
+++ CharacterCreator.Data/ApplicationDbContext.cs
@@ -11,7 +11,5 @@ public class ApplicationDbContext : DbContext
             {    
             }
         public DbSet<CharacterEntity> Character { get; set; }
-        public DbSet<SkillEntity> Skill { get; set; }
-        public DbSet<StoryEntity> Story { get; set; }
-        public DbSet<TeamEntity> Team { get; set; }
+        
     }
