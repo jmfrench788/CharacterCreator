d03473d226f4a8271c45250aab12df1df4bf09c6
Chris Eley <chriseley@Chriss-MacBook-Pro.local>
2022-06-16 15:25:44 -0700

    fixing errors for me

--- CharacterCreator.Data/entities/CharacterEntity.cs
+++ CharacterCreator.Data/entities/CharacterEntity.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CharacterCreator.Data.entities;
+
 public class CharacterEntity
     {
         public int Id { get; set; }
