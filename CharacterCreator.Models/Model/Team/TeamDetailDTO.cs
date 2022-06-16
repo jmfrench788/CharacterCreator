3f12d9bc87d74e540f81d5b08c3e8e6d32b11f1d
Julia French <jmfrench788@gmail.com>
2022-06-16 19:36:19 -0400

    Team/Story DTOs

---
+++ CharacterCreator.Models/Model/Team/TeamDetailDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    public class TeamDetailDTO
+    {
+         public int Id {get;set;}
+        public string TeamName {get;set;}
+        public int StoryId {get;set;}
+        public virtual StoryEntity Story {get;set;}
+
+        public List<CharacterEntity> Characters {get;set;}
+    }
