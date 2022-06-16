3d143b088e6a26384a465bdc7df1f736a7acd207
Rob Rupp <robruppjr@gmail.com>
2022-06-16 19:14:22 -0400

    Setup for DTOS

--- CharacterCreator.Data/entities/CharacterEntity.cs
+++ CharacterCreator.Data/entities/CharacterEntity.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 public class CharacterEntity
     {
+        [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
