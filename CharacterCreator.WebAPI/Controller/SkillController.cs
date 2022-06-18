9aec7ec73796a94a54812fbdee4f76cf3e563f9a
Chris Eley <chriseley@Chriss-MacBook-Pro.local>
2022-06-18 07:32:30 -0700

    added skill controller

--- CharacterCreator.WebAPI/Controller/SkillController.cs
+++ CharacterCreator.WebAPI/Controller/SkillController.cs
@@ -8,11 +8,29 @@ using Microsoft.AspNetCore.Mvc;
     [Route("[controller]")]
     public class SkillController : ControllerBase
     {
+        private readonly ISkillService _skillService;
         private readonly ILogger<SkillController> _logger;
 
         public SkillController(ILogger<SkillController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpPost("Add Skills")]
+        public async Task<IActionResult> CreateSkill([FromBody] SkillCreationDTO request)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var skillCreationResult = await _skillService.CreateSkillAsync(request);
+            if (skillCreationResult)
+            {
+                return Ok("Skill has been added");
+            }
+
+            return BadRequest("Failed to add skill");
         }
 
         
